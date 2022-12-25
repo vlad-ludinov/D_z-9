55168837adf15eda4f8471012135e485b9838def
vlad <vladislavludinov3006@gmail.com>
2022-12-25 18:47:52 +0300

    coordinate input added

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите координаты точки A:");
+Write("x: ");
+bool AxIsInt = int.TryParse(ReadLine(),out int number);
+Write("y: ");
+bool AyIsInt = int.TryParse(ReadLine(),out int number);
+Write("z: ");
+bool AzIsInt = int.TryParse(ReadLine(),out int number);
+WriteLine("Введите координаты точки B:");
+Write("x: ");
+bool BxIsInt = int.TryParse(ReadLine(),out int number);
+Write("y: ");
+bool ByIsInt = int.TryParse(ReadLine(),out int number);
+Write("z: ");
+bool BzIsInt = int.TryParse(ReadLine(),out int number);
