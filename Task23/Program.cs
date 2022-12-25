f2aa09254bc4a5f195eebea3cc04d36e9724376c
vlad <vladislavludinov3006@gmail.com>
2022-12-25 19:25:55 +0300

    number input added

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число: ");
+bool numberIsInt = int.TryParse(ReadLine(), out int number);
