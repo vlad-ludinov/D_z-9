fc68f19a8639ce1a4043118fe531671b566be7fa
vlad <vladislavludinov3006@gmail.com>
2022-12-25 19:42:22 +0300

    Task23 complete

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -8,3 +8,18 @@ if (!numberIsInt)
     WriteLine("Это не число");
     return;
 }
+
+int count = 1;
+while(count!=number)
+{
+    Write($"{Math.Pow(count,3)}, ");
+    if (number >0)
+    {
+        count++;
+    }
+    else
+    {
+        count--;
+    }
+}
+WriteLine(Math.Pow(count,3));
