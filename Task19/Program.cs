5d614bf1e10e523848647874cf64c96eb0b86d7a
vlad <vladislavludinov3006@gmail.com>
2022-12-25 18:30:36 +0300

    Task19 complete

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите пятизначное число: ");
+bool numberIsInt = int.TryParse(ReadLine(),out int number);
+if (!numberIsInt)
+{
+    WriteLine("Это не число");
+    return;
+}
+if (9999<number && number<100000)
+{
+    if (number/10000 == number%10 && number/1000%10 == number/10%10)
+    {
+        WriteLine($"Да, число {number} - палиндром");
+    }
+    else
+    {
+        WriteLine($"Нет, число {number} - не палиндром");
+    }
+}
+else
+{
+    WriteLine("Это не пятизначное число");
+}
